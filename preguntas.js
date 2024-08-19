1a3aac2fdf5ce1f47a14b24a4b60dff3592ae19e
Garcia-Alejo <garcia.alejoet36@gmail.com>
2024-08-19 12:41:03 -0300

    preguntas subidas

---
+++ preguntas.js
@@ -0,0 +1,147 @@
+// preguntas.js
+
+const preguntas = [
+    // Historia
+    {
+      pregunta: "¿Quién fue el primer emperador de Roma?",
+      respuesta: "Augusto"
+    },
+    {
+      pregunta: "¿En qué año comenzó la Primera Guerra Mundial?",
+      respuesta: "1914"
+    },
+    {
+      pregunta: "¿Cuál fue la principal causa de la Revolución Francesa?",
+      respuesta: "Desigualdades sociales y económicas"
+    },
+    {
+      pregunta: "¿Qué civilización construyó las pirámides de Giza?",
+      respuesta: "Egipcia"
+    },
+    {
+      pregunta: "¿Qué reina británica fue conocida como la 'Reina Victoria'?",
+      respuesta: "Victoria"
+    },
+    {
+      pregunta: "¿Qué país fue el primero en conceder el voto a las mujeres?",
+      respuesta: "Nueva Zelanda"
+    },
+    {
+      pregunta: "¿En qué año cayó el Muro de Berlín?",
+      respuesta: "1989"
+    },
+    {
+      pregunta: "¿Quién fue el líder del movimiento de independencia en la India?",
+      respuesta: "Mahatma Gandhi"
+    },
+    {
+      pregunta: "¿Cuál fue la última dinastía imperial de China?",
+      respuesta: "La dinastía Qing"
+    },
+    {
+      pregunta: "¿Qué imperio se conoce por la construcción de Machu Picchu?",
+      respuesta: "El Imperio Inca"
+    },
+  
+    // Deportes
+    {
+      pregunta: "¿Quién es conocido como el 'Rey del Fútbol'?",
+      respuesta: "Pelé"
+    },
+    {
+      pregunta: "¿Qué país ha ganado más Copas del Mundo de la FIFA hasta 2022?",
+      respuesta: "Brasil"
+    },
+    {
+      pregunta: "¿En qué deporte se usa un 'puck'?",
+      respuesta: "Hockey sobre hielo"
+    },
+    {
+      pregunta: "¿Cuál es el nombre del torneo de tenis que se juega en Wimbledon?",
+      respuesta: "Campeonato de Wimbledon"
+    },
+    {
+      pregunta: "¿Qué atleta ganó el mayor número de medallas en los Juegos Olímpicos?",
+      respuesta: "Michael Phelps"
+    },
+    {
+      pregunta: "¿En qué deporte se destaca la disciplina de 'salto con pértiga'?",
+      respuesta: "Atletismo"
+    },
+    {
+      pregunta: "¿Cuál es el equipo de fútbol más exitoso en la historia de la UEFA Champions League?",
+      respuesta: "Real Madrid"
+    },
+    {
+      pregunta: "¿En qué año se celebraron los primeros Juegos Olímpicos modernos?",
+      respuesta: "1896"
+    },
+    {
+      pregunta: "¿Quién tiene el récord de más goles en una sola temporada de La Liga española?",
+      respuesta: "Lionel Messi"
+    },
+    {
+      pregunta: "¿En qué deporte se usa un 'swing' para golpear una bola?",
+      respuesta: "Golf"
+    },
+  
+    // Geografía
+    {
+      pregunta: "¿Cuál es el río más largo del mundo?",
+      respuesta: "Nilo"
+    },
+    {
+      pregunta: "¿En qué continente se encuentra el desierto del Sahara?",
+      respuesta: "África"
+    },
+    {
+      pregunta: "¿Cuál es el país más grande del mundo por superficie?",
+      respuesta: "Rusia"
+    },
+    {
+      pregunta: "¿Qué océano se encuentra al oeste de Australia?",
+      respuesta: "Océano Índico"
+    },
+    {
+      pregunta: "¿Cuál es la capital de Canadá?",
+      respuesta: "Ottawa"
+    },
+    {
+      pregunta: "¿Qué país es conocido por tener la forma de un 'zapato de bota'?",
+      respuesta: "Italia"
+    },
+    {
+      pregunta: "¿Qué gran cadena montañosa se extiende a lo largo de la frontera entre Europa y Asia?",
+      respuesta: "Los Urales"
+    },
+    {
+      pregunta: "¿Cuál es la isla más grande del mundo?",
+      respuesta: "Groenlandia"
+    },
+    {
+      pregunta: "¿En qué continente se encuentra el desierto de Atacama?",
+      respuesta: "América del Sur"
+    },
+    {
+      pregunta: "¿Cuál es el mar más salado del mundo?",
+      respuesta: "El Mar Muerto"
+    },
+  
+    // Preguntas adicionales
+    {
+      pregunta: "¿Cual es el nombre de la señora que atiende la cocina?",
+      respuesta: "Sol"
+    },
+    {
+      pregunta: "Según el rumor conocido en la escuela, ¿qué dinero fue urtado por uno de los directivos?",
+      respuesta: "Cooperadora"
+    },
+    {
+      pregunta: "¿A qué profesor se le filtró un video explícito, de él siendo juguetón con sí mismo?",
+      respuesta: "Alan Uzal"
+    }
+  ];
+  
+  // Exportar el objeto para que pueda ser utilizado en otros archivos
+  module.exports = preguntas;
+  
